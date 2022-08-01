6eeeced7ac0d44e2e6231c44d2c545248476e2a8
Kiran Adhikari <kiranadh1452@gmail.com>
2022-08-01 09:39:52 +0545

    Use Assert class + Code Block re-arranged

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -2,6 +2,7 @@
 
 use Behat\Behat\Context\Context;
 use GuzzleHttp\Client;
+use PHPUnit\Framework\Assert;
 
 /**
  * Defines application features from the specific context.
@@ -30,29 +31,11 @@ class FeatureContext implements Context
     }
 
     /**
-     * @Then the user should get a response code of :arg1
+     * @When the user makes request for a single user data with a name :arg1
      */
-    public function theUserShouldGetAResponseCodeOf($arg1)
+    public function theUserMakesRequestForASingleUserDataWithAName($arg1)
     {
-        $obtainedResponseCode = $this->response->getStatusCode();
-
-        if ($obtainedResponseCode != $arg1) {
-            throw new Exception("Expected response code of $arg1 but got $obtainedResponseCode");
-        }
-    }
-
-    /**
-     * @Then the user should see :arg1 and :arg2 for all objects in the response
-     */
-    public function theUserShouldSeeAndForAllObjectsInTheResponse($arg1, $arg2)
-    {
-        $responseObject = json_decode($this->response->getBody());
-
-        foreach ($responseObject as $id => $data) {
-            if ((!property_exists($data, $arg1) || !(property_exists($data, $arg2)))) {
-                // throw new Exception("Expected $arg1 and $arg2 but got " . json_encode($data));
-            }
-        }
+        $this->response = $this->client->get('/api/user/name=' . $arg1);
     }
 
     /**
@@ -64,22 +47,56 @@ class FeatureContext implements Context
     }
 
     /**
+     * @Then the user should get a response code of :arg1
+     */
+    public function theUserShouldGetAResponseCodeOf($arg1)
+    {
+        $obtainedResponseCode = $this->response->getStatusCode();
+
+        Assert::assertEquals(
+            $arg1,
+            $obtainedResponseCode,
+            __METHOD__ . " Expected response code $arg1 but got $obtainedResponseCode"
+        );
+    }
+
+    /**
+     * @Then the user should see :arg1 and :arg2 for all objects in the response
+     */
+    public function theUserShouldSeeAndForAllObjectsInTheResponse($arg1, $arg2)
+    {
+        $responseObject = json_decode($this->response->getBody());
+
+        foreach ($responseObject as $id => $data) {
+            Assert::assertObjectHasAttribute(
+                $arg1,
+                $data,
+                __METHOD__ . " Expected attribute '$arg1' but found " . json_encode($data)
+            );
+            Assert::assertObjectHasAttribute(
+                $arg2,
+                $data,
+                __METHOD__ . " Expected attribute '$arg2' but found " . json_encode($data)
+            );
+        }
+    }
+
+    /**
      * @Then the user should get :arg1 and :arg2 of a single user in the response
      */
     public function theUserShouldGetAndOfASingleUserInTheResponse($arg1, $arg2)
     {
         $responseObject = json_decode($this->response->getBody());
 
-        if ((!property_exists($responseObject, $arg1) || !(property_exists($responseObject, $arg2)))) {
-            throw new Exception("Expected $arg1 and $arg2 but got " . json_encode($responseObject));
-        }
-    }
-
-    /**
-     * @When the user makes request for a single user data with a name :arg1
-     */
-    public function theUserMakesRequestForASingleUserDataWithAName($arg1)
-    {
-        $this->response = $this->client->get('/api/user/name=' . $arg1);
+        Assert::assertObjectHasAttribute(
+            $arg1,
+            $responseObject,
+            __METHOD__ . " Expected attribute '$arg1' but found " . json_encode($responseObject)
+        );
+        Assert::assertObjectHasAttribute(
+            $arg2,
+            $responseObject,
+            __METHOD__ . " Expected attribute '$arg2' but found " . json_encode($responseObject)
+        );
     }
 }
